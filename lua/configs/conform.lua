2bf659018b40efccf848c9b703c4be23950ba4c3
FUKUOKA Keito <keito.0o0.kirby@gmail.com>
2024-10-07 13:43:31 +0900

    feat(config): enable formatters for lua and python
    This commit updates the conform configuration to enable formatters for
    Lua and Python. The formatters used are "stylua" for Lua and "black"
    and "isort" for Python. Additionally, it removes commented-out
    formatters for CSS and HTML. The format_on_save option is also
    configured with a timeout of 1000ms and lsp_fallback set to true.

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -2,12 +2,9 @@ local options = {
 	formatters_by_ft = {
 		lua = { "stylua" },
 		python = { "black", "isort" },
-		-- css = { "prettier" },
-		-- html = { "prettier" },
 	},
 
 	format_on_save = {
-		-- These options will be passed to conform.format()
 		timeout_ms = 1000,
 		lsp_fallback = true,
 	},
