410bb085ff0ff51e92ef842eebb48977d6b0cda0
Keito Fukuoka <keito.0o0.kirby@gmail.com>
2024-10-22 14:15:20 +0900

    feat(flash): add key mappings for flash and treesitter
    - Added key mapping for "/" to trigger flash.jump() in normal, visual, and
      operator-pending modes.
    - Added key mapping for "//" to trigger flash.treesitter() in normal, visual,
      and operator-pending modes.
    - Enabled search mode in flash configuration.

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,5 +1,26 @@
 return {
 	"folke/flash.nvim",
 	evnt = "VeryLazy",
-	opts = {},
+	---@type Flash.Config
+	opts = {
+		modes = { search = { enabled = true } },
+	},
+	keys = {
+		{
+			"/",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "Flash",
+		},
+		{
+			"//",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").treesitter()
+			end,
+			desc = "Flash Treesitter",
+		},
+	},
 }
